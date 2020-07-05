f885ced132fbb97eb812b03ad84bd19eb7a6fb5c
Samuel Njogu <njogugachigua@gmail.com>
2020-07-05 21:03:17 +0300

    Create light and dark themes

--- MSNews/App.xaml.cs
+++ MSNews/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MSNews.Themes;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +16,7 @@ namespace MSNews
 
         protected override void OnStart()
         {
+            ThemeHelper.GetSystemRequestedTheme();
         }
 
         protected override void OnSleep()
