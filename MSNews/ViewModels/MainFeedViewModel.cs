d8c2abaff0296ee155ee6b57c211eaffbb3d2fe6
Samuel Njogu <njogugachigua@gmail.com>
2020-07-07 12:29:43 +0300

    Setup mvvm pattern for logic implementation for the application

---
+++ MSNews/ViewModels/MainFeedViewModel.cs
@@ -0,0 +1,11 @@
+﻿using PSC.Xamarin.MvvmHelpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MSNews.ViewModels
+{
+    public class MainFeedViewModel : BaseViewModel
+    {
+    }
+}
