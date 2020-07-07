3479f57051fbd9ecdfb12021fd48e40942081fcc
Samuel Njogu <njogugachigua@gmail.com>
2020-07-07 12:56:35 +0300

    Update application files

--- MSNews/Models/Article.cs
+++ MSNews/Models/Article.cs
@@ -1,9 +1,8 @@
-﻿using Newtonsoft.Json;
+﻿using MSNews.Helpers;
+using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
-using System.Text;
 
 namespace MSNews.Models
 {
