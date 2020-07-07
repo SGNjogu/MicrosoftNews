cb583cd3c0206902451f40c9ce7d47d78376f883
Samuel Njogu <njogugachigua@gmail.com>
2020-07-07 10:57:05 +0300

    Create static class to store constants for the application

---
+++ MSNews/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MSNews
+{
+    public static class Constants
+    {
+        public static string NewsAPIKey = "87e176accdb74300ba188c36f7b9de85";
+    }
+}
