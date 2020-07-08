05996df6bb8bd879fb1c3bad18c78f6b1b32ca05
Samuel Njogu <njogugachigua@gmail.com>
2020-07-08 12:16:43 +0300

    Implement carousel view on main feed view

--- MSNews.Android/obj/Debug/90/android/environment.x86_64.s
+++ MSNews.Android/obj/Debug/90/android/environment.x86_64.s
@@ -72,7 +72,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"c5aeb698-3bfa-4c81-a07b-2c061c356be2"
+	.asciz	"186dfdb6-4b18-48ba-a970-0914707a128d"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
