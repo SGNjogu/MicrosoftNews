48d0441fdd27f1a2ae4391c333adfeaa6ab4e4bb
Samuel Njogu <njogugachigua@gmail.com>
2020-07-16 08:38:48 +0300

    Add NewsView ViewCell

---
+++ MSNews/Views/Components/NewComponents/NewsView.xaml.cs
@@ -0,0 +1,29 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MSNews.Views.Components.NewComponents
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NewsView : ViewCell
+    {
+        public NewsView()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnBindingContextChanged()
+        {
+            base.OnBindingContextChanged();
+
+            image.Source = null;
+
+            var bindingContext = BindingContext as Models.Article;
+
+            image.Source = bindingContext.UrlToImage;
+            description.Text = bindingContext.Description;
+            source.Text = $"{bindingContext.Source} . ";
+            published.Text = bindingContext.Published;
+        }
+    }
+}
