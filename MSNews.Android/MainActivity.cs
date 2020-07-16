afad4b7443a63f7f929ac346c191e7b7616fca3d
Samuel Njogu <njogugachigua@gmail.com>
2020-07-16 08:30:18 +0300

    Implement FFImageLoading on android

--- MSNews.Android/MainActivity.cs
+++ MSNews.Android/MainActivity.cs
@@ -29,6 +29,9 @@ namespace MSNews.Droid
             //Implement CardsView
             CardsViewRenderer.Preserve();
 
+            //Implement FFImageLoading
+            FFImageLoading.Forms.Platform.CachedImageRenderer.Init(enableFastRenderer: true);
+
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
