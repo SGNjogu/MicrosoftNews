3b52727dee4eed5d46ddeda77ca9425834f42277
Samuel Njogu <njogugachigua@gmail.com>
2020-07-20 18:26:30 +0300

    Event handler for selected topic

--- MSNews/Views/Pages/MyInterests.xaml.cs
+++ MSNews/Views/Pages/MyInterests.xaml.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -15,6 +11,22 @@ namespace MSNews.Views.Pages
         public MyInterests()
         {
             InitializeComponent();
+            BindingContext = ViewModels.InterestsViewModel.Instance;
+        }
+
+        private async void collectionView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (collectionView.SelectedItem != null)
+            {
+                var selected = e.CurrentSelection.FirstOrDefault() as Models.Interest;
+
+                ViewModels.InterestsViewModel.Instance.CurrentInterest = selected;
+                ViewModels.InterestsViewModel.Instance.CurrentItem = new Models.Tab { Title = selected.Title };
+
+                await Shell.Current.GoToAsync($"interestarticles");
+
+                collectionView.SelectedItem = null;
+            }
         }
     }
 }
